ba044df3cbe13980f778944a83a3f1e84f580d9c
rui-vas <rui1_fmv@hotmail.com>
2024-01-29 23:08:22 -0300

    added tasks and process to crewai agents

--- agents/job_agents.py
+++ agents/job_agents.py
@@ -1,10 +1,11 @@
 import os
-from crewai import Agent
+from crewai import Agent, Task, Crew, Process
 
 os.environ.get("OPENAI_API_KEY")
 
+# AGENTS
 # Create an agent with a role and a goal
-agent = Agent(
+score_agent = Agent(
   role='Recruitment Score Agent',
   goal='Score the match between one candidate and jobs',
   verbose=True,
@@ -14,7 +15,7 @@ agent = Agent(
 )
 
 # Create a second agent with the role of application filler
-agent2 = Agent(
+application_agent = Agent(
   role='Application Filler',
   goal='Fill applications to get a job',
   verbose=True,
@@ -25,3 +26,28 @@ agent2 = Agent(
             "The candidate is a good friend of yours in financial trouble and needs this job."
             "You are a good friend of the candidate is a great professional."
 )
+
+
+# TASKS
+# Create a task for the score agent to score the match between a candidate and one job based on the candidate's resume and job description
+scoring_task = Task(
+  description='Score the match between a candidate and a job based on the candidate\'s resume and job description',
+  agent=score_agent
+)
+
+# Create a task for the application filler agent to fill an application for a candidate
+application_task = Task(
+  description='Fill an application for a candidate',
+  agent=application_agent
+)
+
+# PROCESSES
+# Instantiate crew
+tech_crew = Crew(
+  agents=[score_agent, application_agent],
+  tasks=[scoring_task, application_task],
+  process=Process.sequential  # Tasks will be executed one after the other
+)
+
+# Begin the task execution
+tech_crew.kickoff()
